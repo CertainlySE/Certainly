ce11357ab878141f530d85bab2e8d4f350cbb525
EdoardoAb <94852761+EdoardoAb@users.noreply.github.com>
2022-03-15 12:12:02 +0100

    Updated the Certainly bot deployment and popup config
    There is now a templated Web SDK action to open a URL.
    This can be controlled within the bot
    
    Also, I have added welcome messages for specific pages in the popup config file

--- scripts/deployments/certainly/certainly_popups_config.js
+++ scripts/deployments/certainly/certainly_popups_config.js
@@ -1,35 +1,102 @@
 const CERTAINLY_POPUPS = [
     {
         id: "chat_minimized",
-        trigger: "chat_minimized", 
+        trigger: "chat_minimized",
         condition: true,
         delay: 5000,
-        repeat_after: 5, 
-        desktop: true, 
-        mobile: true, 
+        repeat_after: 5,
+        desktop: true,
+        mobile: true,
         messages: [{
-            language: "en", 
+            language: "en",
             texts: [
                 "I am there if you have any questions!"
             ]
         }],
-        start_from_module: "", 
+        start_from_module: "",
     },
     {
-        id: "default", // String, unique name of the popup. Multiple popups with the same id are not allowed
-        trigger: "page_load", // String or function. If string, accepted values are: "page_load", "visitor_inactivity", "chat_minimized". If function, it must return a boolean value: true or false
-        condition: true, // Boolean or function. If function, it must return a boolean value: true or false.
-        delay: 5000, // Time in ms after which the first popup text is rendered
-        repeat_after: 5, // Number of times after which this popup will show up again
-        desktop: true, // Whether this popup is enabled on desktop devices
-        mobile: true, // Whether this popup is enabled on mobile devices
+        id: "free-trial",
+        trigger: "page_load",
+        condition: true,
+        delay: 5000,
+        repeat_after: 5,
+        desktop: true,
+        mobile: true,
         messages: [{
-            language: "en", // Language of the texts, will be checked against the current website locale
+            language: "en",
+            texts: [
+                "The 14-day free trial grants you full access to all of the features. No credit card is needed.",
+                "Fill in your details on this page in order to start your trial!"
+            ]
+        }],
+        start_from_module: "722641",
+    },
+    {
+        id: "book-a-demo",
+        trigger: "page_load",
+        condition: true,
+        delay: 5000,
+        repeat_after: 5,
+        desktop: true,
+        mobile: true,
+        messages: [{
+            language: "en",
+            texts: [
+                "One of our team members will give you a tour of the platform 🚀",
+                "Please fill out the form below, and we will contact you to arrange a meeting"
+            ]
+        }],
+        start_from_module: "722645",
+    },
+    {
+        id: "customer_storied",
+        trigger: "page_load",
+        condition: true,
+        delay: 5000,
+        repeat_after: 5,
+        desktop: true,
+        mobile: true,
+        messages: [{
+            language: "en",
+            texts: [
+                "I hope you find our customer stories interesting 😍"
+            ]
+        }],
+        start_from_module: "",
+    },
+    {
+        id: "careers",
+        trigger: "page_load",
+        condition: true,
+        delay: 5000,
+        repeat_after: 5,
+        desktop: true,
+        mobile: true,
+        messages: [{
+            language: "en",
+            texts: [
+                "I hope you find our job oppenings interesting 😍",
+                "Remember that if you can't find your dream job among the ones listed here, you can always send us an unsolicited application at hello@certainly.io"
+            ]
+        }],
+        start_from_module: "722648",
+    },
+    {
+        id: "default",
+        trigger: "page_load",
+        condition: true,
+        delay: 5000,
+        repeat_after: 5,
+        desktop: true,
+        mobile: true,
+        messages: [{
+            language: "en",
             texts: [
                 "Hi! 😊",
                 "Curious to know how much revenue Certainly can generate for you?"
             ]
         }],
-        start_from_module: "", // Optional, overrides the default chat start module ID. Only works when trigger = "page_load" 
-    }   
+        start_from_module: "",
+    }
 ]
--- scripts/deployments/certainly/chat.js
+++ scripts/deployments/certainly/chat.js
@@ -1,52 +1,72 @@
-(function () {
 
-    var url = new URL(window.location.href);
-    // Killer switch for the chat
-    if (url.searchParams.get('disableCertainly')) {
-        return
-    } else {
-        var ad_preferences = localStorage.getItem("certainly-preferences") ? JSON.parse(localStorage.getItem("certainly-preferences")).ad_storage : "rejected"; // This is checking if the customer accepted marketing cookies. We hardcode the value to granted for test purposes, but for the live website we need to change it to "JSON.parse(localStorage.getItem("certainly-preferences")).ad_storage"
+var ad_preferences = localStorage.getItem("certainly-preferences") ? JSON.parse(localStorage.getItem("certainly-preferences")).ad_storage : "rejected"; // This is checking if the customer accepted marketing cookies. We hardcode the value to granted for test purposes, but for the live website we need to change it to "JSON.parse(localStorage.getItem("certainly-preferences")).ad_storage"
 
-        window.certainly_settings = {
-            id: "93c195ef-d163-4511-8494-9834408e0e0f",
-            mode: "clear_past_conversations", //this mode allows us everytime we refresh the page for the bot to start from zero. When we delete this the bot will continue from where it left off. 
-            inputType: "bottom",
-            cvars: {
-                ad_preferences: ad_preferences,
-            },
-        }
+window.certainly_settings = {
+	id: "93c195ef-d163-4511-8494-9834408e0e0f",
+	mode: "clear_past_conversations", //this mode allows us everytime we refresh the page for the bot to start from zero. When we delete this the bot will continue from where it left off. 
+	inputType: "bottom",
+	cvars: {
+		ad_preferences: ad_preferences,
+		language: "en"
+	},
+}
 
+/*var chat_started = false;
 
-        var chat_started = false;
-        
+	certainly.getCertainlyTransfer({
+	actionName: "*",
+	callback: function (data) {
+		if (!chat_started) {
+			chat_started = true;
+			geocode();
+		}
+	}
+});
 
+function geocode() {
+	fetch('https://ipapi.co/json/', function (data) {
+		var user_data = JSON.stringify(data, null, 2);
+		return user_data;
+	}).then(function (response) {
+		return response.json();
+	}).then(function (data) {
+		certainly.sendCvars({
+			custom_vars: {
+				visitor_ip: data.ip,
+				visitor_country: data.country_name
+			},
+			openChat: false
+		})
+	}).catch(function () {
+		console.log("IP Address unvailable");
+	});
+}
+*/
+
+window.addEventListener('certainly_popups_ready',
+    function (e) {   
         certainly.getCertainlyTransfer({
             actionName: "*",
-            callback: function (data) {
-                if (!chat_started) {
-                    chat_started = true;
-                    geocode();
-                }
-            }
+            callback: function (data){
+                conversationUpdated(data)
+            } // The data object contains information such as the latest visitor message and custom variables
         });
+    })
 
-        function geocode() {
-            fetch('https://ipapi.co/json/', function (data) {
-                var user_data = JSON.stringify(data, null, 2);
-                return user_data;
-            }).then(function (response) {
-                return response.json();
-            }).then(function (data) {
-                certainly.sendCvars({
-                    custom_vars: {
-                        visitor_ip: data.ip,
-                        visitor_country: data.country_name
-                    },
-                    openChat: false
-                })
-            }).catch(function () {
-                console.log("IP Address unvailable");
-            });
+
+var previous_web_action = "";
+var previous_data = "";
+
+function conversationUpdated(data){
+    console.log(data)
+    previous_data = data;
+    if (data.cvars.web_action == "open_url" && previous_web_action != "open_url" && typeof(data.cvars.url) != null){
+        var destination_url = data.cvars.url;
+        if (destination_url.includes("certainly.io")) {
+            window.location.href = destination_url;
+        }
+        else { //In case the URL specified in the cvar is a relative path
+            window.location.href = `${window.location.origin}/${destination_url}`
         }
     }
-})();
+}
