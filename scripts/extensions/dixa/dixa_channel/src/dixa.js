ca496a52c53f8ec6d85798f6b9f71d9928f29acb
EdoardoAb <94852761+EdoardoAb@users.noreply.github.com>
2022-04-04 11:41:01 +0200

    Removed console.logs from Dixa

--- scripts/extensions/dixa/dixa_channel/src/dixa.js
+++ scripts/extensions/dixa/dixa_channel/src/dixa.js
@@ -7,7 +7,7 @@ var dixa_settings = {
 	previous_event: ""
 }
 
-console.log("Hello")
+
 
 _dixa("api.showWidget", "{\"show_widget\":false, \"show_contact_form\":false}");
 /*
@@ -25,7 +25,6 @@ document.head.append(style);
 
 function synchDixa(){
 	dixa_settings.metadata = JSON.parse(localStorage.getItem("dixa-widget"))
-	console.log("meta", dixa_settings.metadata)
 	if(dixa_settings.metadata != null){
 		if (dixa_settings.metadata.conversation.currentConversation){
 			dixa_settings.user_id = dixa_settings.metadata.user.currentUser.id; 
@@ -48,7 +47,6 @@ _dixa(
 	"api.subscribe",
 	"onConversationStarted",
 	function(){
-		console.log("Conversation started");
 		
 	}
 );
@@ -64,7 +62,6 @@ _dixa(
 	"api.subscribe",
 	"onAgentAssigned",
 	function(payload){
-		console.log("Agent joined: ", payload)
 		dixa_settings.agent_id = payload.agent.id;
 		if (certainly_settings.dixa_events && certainly_settings.dixa_events.agent_joined_message && dixa_settings.previous_event != payload){
 			dixa_settings.previous_event = payload;
@@ -103,7 +100,6 @@ _dixa(
 	"api.subscribe",
 	"onAgentUnassigned",
 	function(payload){
-		console.log("Agent left: ", payload)
 		if (certainly_settings.dixa_events && certainly_settings.dixa_events.agent_left_message && dixa_settings.previous_event != payload){
 			dixa_settings.previous_event = payload;
 			certainly.sendMessage(
@@ -141,7 +137,6 @@ _dixa(
 	"api.subscribe",
 	"onConversationEnded",
 	function(){
-		console.log("Conversation ended");
 		if (certainly_settings.dixa_events && certainly_settings.dixa_events.agent_left_message && dixa_settings.previous_event != "chat_ended"){
 			dixa_settings.previous_event = "chat_ended";
 			certainly.sendMessage(
@@ -197,7 +192,6 @@ function trackDixaChat(){
 		"api.subscribe",
 		"onMessageAdded",
 		function(payload){						
-			console.log("Message added: ", payload)
 			if (dixa_settings.agent_id == "") {
 				dixa_settings.metadata = JSON.parse(localStorage.getItem("dixa-widget"))
 				dixa_settings.agent_id = dixa_settings.metadata.conversation.assignedAgent.agent_id
@@ -207,7 +201,6 @@ function trackDixaChat(){
 			}
 			
 			if (payload.author.id == dixa_settings.agent_id && payload.author.name != "dixa-bot" && payload.message.text != dixa_settings.previous_agent_message){
-				console.log("Sending human agent message to Certainly")
 				dixa_settings.previous_agent_message = payload.message.text;
 				// First, simulates a "Bot is typing" event
 				var messageWaitingTime = 125 * payload.message.text.length; // In ms
@@ -220,12 +213,10 @@ function trackDixaChat(){
 					},
 					webchatKey: "1"
 					}, function(){
-							console.log("callback");
 						}
 				)
 				// After the "Bot is typing" time has elapsed, post the agent message to Certainly
 				setTimeout(function() {
-					console.log("Posting message to Certainly");
 
 					certainly.dataCertainlyTransfer({ 
 						data: { 
@@ -247,12 +238,10 @@ function trackDixaChat(){
 certainly.getCertainlyTransfer({
 	actionName: "*",
 	callback: function(data) {
-		console.log(data)
 			if (data.cvars == "") {
 					console.log("You need to enable your Certainly bot to expose custom variables")
 			}
 			if (data.cvars.dixa && data.cvars.dixa == "start_chat") {
-					console.log("Starting a chat on Dixa")
 					if (window._dixa) {
 						var visitor_name = data.cvars.visitor_name ? data.cvars.visitor_name : 'Anonymous Visitor';
 						var visitor_email = data.cvars.visitor_email ? data.cvars.visitor_email :'visitor@email.com';
